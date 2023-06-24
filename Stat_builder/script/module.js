6429b47c02e8b7a01f1178ffed1734a1c9c163ca
Soulxmatt <matias.munoz.coquidan@gmail.com>
2023-06-24 04:39:47 -0400

    "Formulas Added"

--- Stat_builder/script/module.js
+++ Stat_builder/script/module.js
@@ -16,6 +16,18 @@ function getTotal(exception=undefined){
     }  
     return sum;
 }
+function statistics_formula(EV,level,Nature,Base,IV){
+    return ((Math.floor(0.01*(2* Base+IV+Math.floor(0.25*EV))*level) + 5)*Nature)
+}
+function hp_formula(EV,level,Base,IV){
+    return Math.floor(0.01*(2*Base+IV+Math.floor(0.25*EV))*level)+level+10
+}
+function max_reached(key){
+    if(getTotal()>MAX_VALUE){
+        stats[key].value=MAX_VALUE-getTotal(key)
+    }
+    document.getElementById(`${key}_ev_label`).innerText=stats[key].value;
+}
 var stats={
     hp:document.getElementById("hp_ev"),
     atk:document.getElementById("atk_ev"),
@@ -28,12 +40,18 @@ const MAX_VALUE=512;
 for (const key in stats) {
     if (stats.hasOwnProperty(key)) {
         document.getElementById(`${key}_ev_label`).innerText=stats[key].value;
-        stats[key].addEventListener('input',()=>{
-        if(getTotal()>MAX_VALUE){
-            stats[key].value=MAX_VALUE-getTotal(key)
+        if(key!="hp"){
+            stats[key].addEventListener('input',()=>{
+                max_reached(key)
+                console.log(statistics_formula(stats[key].value,100,1,parseInt(pkdex[selector.value].stats[key]),31))
+            })
+        }else{
+            stats[key].addEventListener('input',()=>{
+                max_reached(key)
+                console.log(hp_formula(stats[key].value,100,parseInt(pkdex[selector.value].stats[key]),31))
+            })
         }
-        document.getElementById(`${key}_ev_label`).innerText=stats[key].value;
-      })
+
     }
   }
 selector.addEventListener('change',function(){
@@ -41,4 +59,3 @@ selector.addEventListener('change',function(){
 })
 
 displayer.src=`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pkdex[selector.value]['dex number']}.png`;
-
